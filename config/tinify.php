3f80a259537259ba2e4abcf5a37e9994acc7ec76
Ratha8 <voeun.ratha@gmail.com>
2019-10-24 16:36:12 +0700

    Update tinify.php

--- config/tinify.php
+++ config/tinify.php
@@ -2,4 +2,8 @@
 
 return [
 	'apikey' => env('TINIFY_APIKEY'),
+        's3_key' => env('S3_KEY'),
+        's3_secret' => env('S3_SECRET'),
+        's3_region' => env('S3_REGION'),
+        's3_bucket' => env('S3_BUCKET'),
 ];
